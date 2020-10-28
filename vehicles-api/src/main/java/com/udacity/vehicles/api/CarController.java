50990e95e417d220b8b3e1396484c2d17adab31a
John Wolfe <jwolfe890@gmail.com>
2020-10-27 21:54:25 -0500

    updated car action

--- vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
@@ -58,14 +58,8 @@ class CarController {
      */
     @GetMapping("/{id}")
     Resource<Car> get(@PathVariable Long id) {
-
-        /**
-         * TODO: Use the `findById` method from the Car Service to get car information.
-         * TODO: Use the `assembler` on that car and return the resulting output.
-         *   Update the first line as part of the above implementing.
-         */
         Car car = carService.findById(id);
-        return assembler.toResource(new Car());
+        return assembler.toResource(car);
     }
 
     /**
